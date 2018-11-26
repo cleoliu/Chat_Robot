e7d280dbaffc5e54ad92ee65057148872ac980f8
cleo <second934@gmail.com>
2018-11-26 22:49:43 +0800

    shop

--- fun/resource.py
+++ fun/resource.py
@@ -1,15 +1,15 @@
 from selenium.webdriver.common.by import By
 
-class tmall:
+class tmall(object):
     Item = (By.XPATH, '//*[@class="tb-detail-hd"]/h1')
     Price = (By.XPATH, '//*[@class="tm-promo-price"]/span')
-    Option = (By.CSS_SELECTOR, 'ul.tm-clear.J_TSaleProp.tb-img > li:nth-child('+i+') > a > span')
-    Image = (By.XPATH , '//*[@id="J_UlThumb"]/li['+i+']/a') #5個
+    #Option = (By.CSS_SELECTOR, 'ul.tm-clear.J_TSaleProp.tb-img > li:nth-child('+i+') > a > span')
+    #Image = (By.XPATH , '//*[@id="J_UlThumb"]/li['+i+']/a') #5個
 
 #.get_attribute('href')
 
-class taobao:
+class taobao(object):
     Item = (By.XPATH, '//*[@id="J_Title"]/h3')
     Price = (By.XPATH, '//*[@id="J_StrPrice"]/em[2]')
-    Option = (By.CSS_SELECTOR, 'ul.J_TSaleProp.tb-img.tb-clearfix > li:nth-child(1) > a > span')
-    Image = (By.XPATH , '//*[@id="J_UlThumb"]/li['+i+']/div/a/img') #6個
+    #Option = (By.CSS_SELECTOR, 'ul.J_TSaleProp.tb-img.tb-clearfix > li:nth-child(1) > a > span')
+    #Image = (By.XPATH , '//*[@id="J_UlThumb"]/li['+i+']/div/a/img') #6個
--- wechat_robot.py
+++ wechat_robot.py
@@ -5,7 +5,7 @@ import time
 import requests
 import json
 from translate import translator
-from shop import shop_find
+#from shop import shop_find
 
 
 def post_tulin_robot(text): #-- POST --#
@@ -14,8 +14,8 @@ def post_tulin_robot(text): #-- POST --#
         translate_word = text.replace('翻譯', '')
         translate_reuslt = translator(translate_word)
         return translate_reuslt
-    if "淘♂寳" in text:
-        shop_find(text)
+    #if "淘♂寳" in text:
+        #shop_find(text)
     else :  # 其他訊息則post圖靈回應
         url = 'http://www.tuling123.com/openapi/api'
         data = {
@@ -27,14 +27,25 @@ def post_tulin_robot(text): #-- POST --#
         response = requests.post(url, data=data).json()
         return response["text"]
 
+# --自動回覆好友--
+# @itchat.msg_register('Text')    #-- itchat裝飾器 --#
+# def text_reply(msg):    #-- 接收wechat用戶發來的訊息(排除自己發的) --#
+#     repa = post_tulin_robot(msg['Text'])
+#     return  u"[我是機器人]{}".format(repa) # 回覆給微信好友
 
-@itchat.msg_register('Text')    #-- itchat裝飾器 --#
-def text_reply(msg):    #-- 接收wechat用戶發來的訊息(排除自己發的) --#
-    repa = post_tulin_robot(msg['Text'])
-    return  u"[我是機器人]{}".format(repa)
-
+# 回覆給指定對象
+@itchat.msg_register(itchat.content.TEXT)
+def auto_reply(msg):
+    # 搜尋微信好友
+    readFriend=itchat.search_friends(name='Cleo')
+    readFriendsName=readFriend[0]['UserName'] #@03243591003c97d1f11f4db3b80b8dc779f51b0db57a1ada9c1a488dc3c5b10c
+    # 列印好友回覆的資訊
+    print("message:%s"%msg['Text'])
+    reply=post_tulin_robot(msg['Text'])
+    if msg['FromUserName']==readFriendsName and msg['ToUserName']==readFriendsName:
+        itchat.send(reply,toUserName=readFriendsName)
 
 if __name__ == '__main__':
-    itchat.auto_login(enableCmdQR=2)# Login QRcode
+    itchat.auto_login(enableCmdQR=1, hotReload=True)# Login QRcode
     #itchat.send('測試發送內容', toUserName='filehelper')
     itchat.run(debug=True)    
